5cca550c77e9032e67323ea7c006e517afb8c47f
Randy Ibarrola <randy.ibarrola@gmail.com>
2014-02-10 16:12:59 -0500

    cambios para agregar candidatos

--- src/Encuesta/FrontendBundle/Controller/VotacionController.php
+++ src/Encuesta/FrontendBundle/Controller/VotacionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Encuesta\ModeloBundle\Form\EventoType;
 use Encuesta\ModeloBundle\Entity\Evento;
+use Encuesta\ModeloBundle\Entity\Candidato;
 
 class VotacionController extends Controller
 {
@@ -22,38 +23,50 @@ class VotacionController extends Controller
             $form->bind($peticion);
             if($form->isValid()){
                 $evento = $form->getData();   
+                if($form['imagen']->getData()){
                 $extension = $form['imagen']->getData()->guessExtension();
-                if( in_array( $extension, array('png', 'jpg', 'jpeg', 'gif', 'bmp') ) ) {
-                    if(!is_dir($evento->getUploadDir()))      
-                               mkdir($evento->getUploadDir(), 0777);                       
-                    
-                    $nombre = md5(time());
-                    $form['imagen']->getData()->move($evento->getUploadDir(), $nombre.'.'.$extension);
-                    $evento->setImagen($nombre.$extension);
-                }         
+                    if( in_array( $extension, array('png', 'jpg', 'jpeg', 'gif', 'bmp') ) ) {
+                        if(!is_dir($evento->getUploadDir()))      
+                                   mkdir($evento->getUploadDir(), 0777);                       
+
+                        $nombre = md5(time());
+                        $form['imagen']->getData()->move($evento->getUploadDir(), $nombre.'.'.$extension);
+                        $evento->setImagen($nombre.$extension);
+                    }   
+                }
                 $evento->setCreador($this->getUser());
                 $em->persist($evento);
                 $em->flush();    
                 
-                return $this->redirect($this->generateUrl('frontend_agregar_candidato'));
+                return $this->redirect($this->generateUrl('frontend_agregar_candidato', array('id'=>$evento->getId())));
                 
             }
         }
         
-        $eventos = $em->getRepository('ModeloBundle:Evento')->getEventosActivos();
+        $todosEventos = $em->getRepository('ModeloBundle:Evento')->getEventosActivos();
+        $misEventos = $em->getRepository('ModeloBundle:Evento')->getEventosActivos($this->getUser());
         return $this->render('FrontendBundle:Votacion:votaciones.html.twig', array(
             'form' => $form->createView(),
-            'eventos' => $eventos
+            'eventos' => $todosEventos            
         ));         
         
     }
     
     public function agregarCandidatoAction()
     {
-        return $this->render('FrontendBundle:Votacion:agregarCandidato.html.twig', array(
-           
-           
-        ));        
+        $em = $this->getDoctrine()->getManager();    
+        $id = $this->getRequest()->get('id');
+        $evento = $id ? $em->getRepository('ModeloBundle:Evento')->find($id)  : null;
+        if($evento) {
+            
+            $candidato = new Candidato();
+            return $this->render('FrontendBundle:Votacion:agregarCandidato.html.twig', array(
+                'evento' => $evento
+            ));
+        }
+        
+        throw $this->createNotFoundException();
+                
     }
 }
 
